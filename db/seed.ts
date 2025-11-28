63486beab82b59ab87bfbf46a5857d54a1e031de
DanielZR05 <danielzanon2005@gmail.com>
2025-11-28 09:52:14 +0100

    Repasar lo que hemos visto hoy

--- db/seed.ts
+++ db/seed.ts
@@ -1,9 +1,15 @@
+import "dotenv/config";
 import { PrismaClient } from "@/lib/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 import sampleData from "./sample-data";
 
+const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL,
+})
+
 async function main() {
-    const prisma = new PrismaClient();
-    await prisma.product.createMany({data: sampleData.products});
+    const prisma = new PrismaClient({ adapter });
+    await prisma.product.createMany({ data: sampleData.products });
     console.log("Productos introducidos en la base de datos");
 }
 
