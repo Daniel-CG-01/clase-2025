ce2001a35dbe7189987581a24b83283a41776519
DanielZR05 <danielzanon2005@gmail.com>
2026-01-12 14:43:11 +0100

    12/01/2026

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,3 +1,8 @@
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Tienda Next";
 export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || "Este es el proyecto Next de práctica en DAW";
 export const SERVER_URL = process. env.NEXT_PUBLIC_SERVER_URL || "https://localhost:3000";
+export const signUpDefaultValues = {
+    name: "daniel",
+    email: "daniel@daniel.com",
+    password: "123456",
+}
