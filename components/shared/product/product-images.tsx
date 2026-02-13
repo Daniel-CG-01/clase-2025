7c51fa3704ae1098221e85beeca9b0e5780f8283
Daniel <danielzanon2005@gmail.com>
2026-02-13 23:17:25 +0100

    He mejorado la perspectiva de la imagen del producto

--- components/shared/product/product-images.tsx
+++ components/shared/product/product-images.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React, { useState } from 'react';
 import Image from 'next/image';
-import { Button } from '@/components/ui/button';
 
 interface ProductImagesProps {
   images: string[];
@@ -9,12 +8,16 @@ interface ProductImagesProps {
 
 export default function ProductImages({ images }: ProductImagesProps) {
     const [selectedImage, setSelectedImage] = useState<string>(images?.[0] || '');
+
+    // Si no hay imágenes, no se muestra nada
+    if (!images || images.length === 0) return null;
   return (
     <div className="flex flex-col gap-4">
+      {/* IMAGEN PRINCIPAL */}
       <div className="flex justify-between">
         <Image
             src={selectedImage}
-            alt="imagen de producto seleccionada"
+            alt="imagen del producto seleccionado"
             width={1000}
             height={1000}
             className="min-h-[300px] object-cover object-center"
@@ -22,14 +25,14 @@ export default function ProductImages({ images }: ProductImagesProps) {
         />
       </div>
 
-      {/* Tipos de imagen */}
+      {/* IMÁGENES SECUNDARIAS */}
       {images.length > 1 &&
-        <div className="flex gap-2 over-flow-x-auto">
+        <div className="flex justify-center items-center gap-2 over-flow-x-auto">
           {images.map((image, index) => (
-            <Button
+            <div
               key={index}
               onClick={() => setSelectedImage(image)}
-              className={`relative shrink-0 w-0 h-0 rounded-md border-2 transition-all ${
+              className={`relative shrink-0 rounded-md border-2 transition-all ${
                 selectedImage === image
                   ? 'border-black'
                   : 'border-gray-300 hover:border-gray-400'
@@ -42,7 +45,7 @@ export default function ProductImages({ images }: ProductImagesProps) {
                 height={100}
                 className="w-full h-full object-cover rounded-md"
               />
-            </Button>
+            </div>
           ))}
         </div>
       }
